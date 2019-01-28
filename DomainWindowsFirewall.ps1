4b540cdd074474328987ea17e54817d5cd81df46
tg12 <tg12@users.noreply.github.com>
2019-01-28 10:18:47 +0000

    Update DomainWindowsFirewall.ps1

--- DomainWindowsFirewall.ps1
+++ DomainWindowsFirewall.ps1
@@ -1,4 +1,4 @@
-
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned 
 #DNS: UDP (1:65535) / (53), TCP (1:65535) / (53)
 #DFSR_RPC: TCP (49152:65535) / (49152:65535)
 #FRS_RPC: TCP (49152:65535) / (49152:65535)
@@ -28,66 +28,70 @@
 
 # 88/TCP/UDP
 New-NetFirewallRule -DisplayName “Domain TCP 88” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 88 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 88” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 88 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 88” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 88 -Action allow
 New-NetFirewallRule -DisplayName “Domain UDP 88” -Profile Any -Direction Inbound –Protocol UDP –LocalPort 88 -Action allow
-New-NetFirewallRule -DisplayName “Domain UDP 88” -Profile Any -Direction Outbound –Protocol UDP –LocalPort 88 -Action allow
+New-NetFirewallRule -DisplayName “Domain UDP 88” -Profile Any -Direction Outbound –Protocol UDP –RemotePort 88 -Action allow
 
 # 389/TCP/UDP
 New-NetFirewallRule -DisplayName “Domain TCP 389” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 389 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 389” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 389 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 389” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 389 -Action allow
 New-NetFirewallRule -DisplayName “Domain UDP 389” -Profile Any -Direction Inbound –Protocol UDP –LocalPort 389 -Action allow
-New-NetFirewallRule -DisplayName “Domain UDP 389” -Profile Any -Direction Outbound –Protocol UDP –LocalPort 389 -Action allow
+New-NetFirewallRule -DisplayName “Domain UDP 389” -Profile Any -Direction Outbound –Protocol UDP –RemotePort 389 -Action allow
 
 # 636/TCP
 New-NetFirewallRule -DisplayName “Domain TCP 636” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 636 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 636” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 636 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 636” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 636 -Action allow
 
 # 3268/TCP
 New-NetFirewallRule -DisplayName “Domain TCP 3268” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 3268 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 3268” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 3268 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 3268” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 3268 -Action allow
 
 # 3269/TCP
 New-NetFirewallRule -DisplayName “Domain TCP 3269” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 3269 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 3269” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 3269 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 3269” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 3269 -Action allow
 
 # 445/TCP
 New-NetFirewallRule -DisplayName “Domain TCP 445” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 445 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 445” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 445 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 445” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 445 -Action allow
 
 # 135/TCP
 New-NetFirewallRule -DisplayName “Domain TCP 135” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 135 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 135” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 135 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 135” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 135 -Action allow
 
 # 137/UDP
 New-NetFirewallRule -DisplayName “Domain UDP 137” -Profile Any -Direction Inbound –Protocol UDP –LocalPort 137 -Action allow
-New-NetFirewallRule -DisplayName “Domain UDP 137” -Profile Any -Direction Outbound –Protocol UDP –LocalPort 137 -Action allow
+New-NetFirewallRule -DisplayName “Domain UDP 137” -Profile Any -Direction Outbound –Protocol UDP –RemotePort 137 -Action allow
 
 # 138/UDP
 New-NetFirewallRule -DisplayName “Domain UDP 389” -Profile Any -Direction Inbound –Protocol UDP –LocalPort 138 -Action allow
-New-NetFirewallRule -DisplayName “Domain UDP 389” -Profile Any -Direction Outbound –Protocol UDP –LocalPort 138 -Action allow
+New-NetFirewallRule -DisplayName “Domain UDP 389” -Profile Any -Direction Outbound –Protocol UDP –RemotePort 138 -Action allow
 
 # 139/TCP
 New-NetFirewallRule -DisplayName “Domain TCP 139” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 139 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 139” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 139 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 139” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 139 -Action allow
 
 # 464/TCP/UDP
 New-NetFirewallRule -DisplayName “Domain TCP 464” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 464 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 464” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 464 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 464” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 464 -Action allow
 New-NetFirewallRule -DisplayName “Domain UDP 464” -Profile Any -Direction Inbound –Protocol UDP –LocalPort 464 -Action allow
-New-NetFirewallRule -DisplayName “Domain UDP 464” -Profile Any -Direction Outbound –Protocol UDP –LocalPort 464 -Action allow
+New-NetFirewallRule -DisplayName “Domain UDP 464” -Profile Any -Direction Outbound –Protocol UDP –RemotePort 464 -Action allow
 
 # 123/UDP
 New-NetFirewallRule -DisplayName “Domain UDP 123” -Profile Any -Direction Inbound –Protocol UDP –LocalPort 123 -Action allow
-New-NetFirewallRule -DisplayName “Domain UDP 123” -Profile Any -Direction Outbound –Protocol UDP –LocalPort 123 -Action allow
+New-NetFirewallRule -DisplayName “Domain UDP 123” -Profile Any -Direction Outbound –Protocol UDP –RemotePort 123 -Action allow
 
 # 53/TCP/UDP
 New-NetFirewallRule -DisplayName “Domain TCP 53” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 53 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 53” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 53 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 53” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 53 -Action allow
 New-NetFirewallRule -DisplayName “Domain UDP 53” -Profile Any -Direction Inbound –Protocol UDP –LocalPort 53 -Action allow
-New-NetFirewallRule -DisplayName “Domain UDP 53” -Profile Any -Direction Outbound –Protocol UDP –LocalPort 53 -Action allow
+New-NetFirewallRule -DisplayName “Domain UDP 53” -Profile Any -Direction Outbound –Protocol UDP –RemotePort 53 -Action allow
 
 # 49152-65535/TCP
 New-NetFirewallRule -DisplayName “Domain TCP 49152-65535” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 49152-65535 -Action allow
-New-NetFirewallRule -DisplayName “Domain TCP 49152-65535” -Profile Any -Direction Outbound –Protocol TCP –LocalPort 49152-65535 -Action allow
+New-NetFirewallRule -DisplayName “Domain TCP 49152-65535” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 49152-65535 -Action allow
 
-
+#Opt WSUS
+New-NetFirewallRule -DisplayName “WSUS TCP 8530” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 8530 -Action allow
+New-NetFirewallRule -DisplayName “WSUS TCP 8530” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 8530 -Action allow
+New-NetFirewallRule -DisplayName “WSUS TCP 8531” -Profile Any -Direction Inbound –Protocol TCP –LocalPort 8531 -Action allow
+New-NetFirewallRule -DisplayName “WSUS TCP 8530” -Profile Any -Direction Outbound –Protocol TCP –RemotePort 8531 -Action allow
